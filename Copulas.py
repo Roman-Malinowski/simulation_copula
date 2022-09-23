0b38eed51f9f81103ae824dfb54dfba8a2bdcf28
Roman Malinowski <roman.malinowski@mines-paristech.fr>
2022-09-23 17:40:20 +0200

    First draft, should test

---
+++ Copulas.py
@@ -0,0 +1,89 @@
+import warnings
+import numpy as np
+
+
+def min_copula(u: float, v: float) -> float:
+    """
+    The minimun copula
+    :param u: a float between 0 and 1
+    :param v: a float between 0 and 1
+    :return: a float between 0 and 1 corresponding to C(u,v)
+    """
+    if 0. > u or 1. < u or 0 > v or 1 < v:
+        warnings.warn("u and v should be between 1 and 0. u=%s ; b=%s\\"
+                      "Cropping the values." % (u, v), UserWarning)
+        u = max(0., u)
+        u = min(1., u)
+        v = max(0., v)
+        v = min(1., v)
+    return min(u, v)
+
+
+def lukaciewicz_copula(u: float, v: float) -> float:
+    """
+    The lukaciewicz copula
+    :param u: a float between 0 and 1
+    :param v: a float between 0 and 1
+    :return: a float between 0 and 1 corresponding to C(u,v)
+    """
+    if 0. > u or 1. < u or 0 > v or 1 < v:
+        warnings.warn("u and v should be between 1 and 0. u=%s ; b=%s\\"
+                      "Cropping the values." % (u, v), UserWarning)
+        u = max(0., u)
+        u = min(1., u)
+        v = max(0., v)
+        v = min(1., v)
+    return max(0., u + v - 1)
+
+
+def ali_mikhail_haq_copula(u, v, theta):
+    if 0. > u or 1. < u or 0 > v or 1 < v:
+        warnings.warn("u and v should be between 1 and 0. u=%s ; b=%s\\"
+                      "Cropping the values." % (u, v), UserWarning)
+        u = max(0., u)
+        u = min(1., u)
+        v = max(0., v)
+        v = min(1., v)
+    if theta < -1 or theta >= 1:
+        err = "Theta should be in [-1, 1[: theta=%s" % theta
+        raise ValueError(err)
+    return u * v / (1 - theta * (1 - u) * (1 - v))
+
+
+def clayton_copula(u, v, theta):
+    if 0. > u or 1. < u or 0 > v or 1 < v:
+        warnings.warn("u and v should be between 1 and 0. u=%s ; b=%s\\"
+                      "Cropping the values." % (u, v), UserWarning)
+        u = max(0., u)
+        u = min(1., u)
+        v = max(0., v)
+        v = min(1., v)
+    if theta < -1 or theta == 0:
+        err = "Theta should be in [-1, infty[ / {0}: theta=%s" % theta
+        raise ValueError(err)
+    return max(u**(-theta) + v**(-theta) - 1, 0)**(-1/theta)
+
+
+def gumbel_copula(u, v, theta):
+    if 0. > u or 1. < u or 0 > v or 1 < v:
+        warnings.warn("u and v should be between 1 and 0. u=%s ; b=%s\\"
+                      "Cropping the values." % (u, v), UserWarning)
+        u = max(0., u)
+        u = min(1., u)
+        v = max(0., v)
+        v = min(1., v)
+    if theta <= 0 or theta > 1:
+        err = "Theta should be in ]0, 1]: theta=%s" % theta
+        raise ValueError(err)
+    return u*v*np.exp(-theta*np.log(u)*np.log(v))
+
+
+def product_copula(u, v, theta):
+    if 0. > u or 1. < u or 0 > v or 1 < v:
+        warnings.warn("u and v should be between 1 and 0. u=%s ; b=%s\\"
+                      "Cropping the values." % (u, v), UserWarning)
+        u = max(0., u)
+        u = min(1., u)
+        v = max(0., v)
+        v = min(1., v)
+    return u*v
--- ordering_focal_set_importance.py
+++ ordering_focal_set_importance.py
@@ -1,4 +1,6 @@
 import types
+import numpy as np
+import itertools
 
 import pandas as pd
 from Copulas import *
@@ -64,8 +66,10 @@ def joint_mass(mass_x: pd.DataFrame, mass_y: pd.DataFrame, order_x: pd.DataFrame
 
     Copula: a function that takes as argument two floats between 0 and 1 and returns the copula on those numbers
 
-    return mass_joint: pd.DataFrame. A dataframe with MultiIndex (product of 'Focal sets' from mass_x and mass_y) and column 'mass'
+    return mass_joint: pd.DataFrame. A dataframe with MultiIndex (product of 'Focal sets' from mass_x and mass_y)
+    and column 'mass'
     """
+    # TODO: Check that the focal sets created by mass_from_possibility are written the same way as order_x and order_y
     multi_index = pd.MultiIndex.from_product([mass_x["Focal sets"], mass_y["Focal sets"]], names=("X", "Y"))
     mass_joint = pd.DataFrame(columns=["mass"], index=multi_index)
 
@@ -84,14 +88,151 @@ def joint_mass(mass_x: pd.DataFrame, mass_y: pd.DataFrame, order_x: pd.DataFrame
     return mass_joint
 
 
+def probability_range_from_poss(poss: dict) -> pd.DataFrame:
+    """
+    Create a DataFrame with probability ranges for atoms, with columns being Necessity and Plausibility
+    poss: A dictionary with atoms being the keys and possibility the values
+    return p: pd.DataFrame. Columns=["Nec", "Pl"], rows are atoms
+    Example
+                    Nec     Pl
+    x1              0.5     1
+    x2              0       0.2
+    x3              0       0.5
+    """
+    mass = mass_from_possibility(poss)
+
+    p = pd.DataFrame(columns=["Nec", "Pl"], index=pd.Index(poss.keys()))
+
+    for atom in p.index:
+        # Kind of useless as there will always be only one atom to be included
+        inclusion = [k in atom for k in mass["Focal sets"]]
+        p.loc[atom, "Nec"] = mass[inclusion]["mass"].sum()
+
+        intersection = [atom in k for k in mass["Focal sets"]]
+        p.loc[atom, "Pl"] = mass[intersection]["mass"].sum()
+    return p
+
+
+def generator_probability(prob_range: pd.DataFrame) -> pd.DataFrame:
+    """
+    Generator function for probabilities. Yields a sampled probability respecting the probability ranges on atoms
+    prob_range: pd.DataFrame.  Columns=["Nec", "Pl"]. 3 Rows.  Obtained from probability_range_from_poss
+    Example
+                    Nec     Pl
+    x1              0.5     1
+    x2              0       0.2
+    x3              0       0.5
+    """
+
+    # TODO How to do the same with any number of atoms?
+    p = pd.DataFrame(columns=["P"], index=prob_range.index)
+    for x1 in np.linspace(prob_range.loc[prob_range.index[0], "Nec"], prob_range.loc[prob_range.index[0], "Pl"],
+                          num=11):
+        pl_2 = min(1 - x1, prob_range.loc[prob_range.index[1], "Pl"])
+        if pl_2 < prob_range.loc[prob_range.index[1], "Nec"]:
+            p["P"] = [x1, 0, 0]
+            yield p
+        else:
+            for x2 in np.linspace(prob_range.loc[prob_range.index[1], "Nec"], pl_2, num=11):
+                pl_3 = min(1 - x1 - x2, prob_range.loc[prob_range.index[2], "Pl"])
+                if pl_3 < prob_range.loc[prob_range.index[2], "Nec"]:
+                    p["P"] = [x1, x2, 0]
+                    yield p
+                else:
+                    for x3 in np.linspace(prob_range.loc[prob_range.index[2], "Nec"], pl_3, num=11):
+                        p["P"] = [x1, x2, x3]
+                        yield p
+    yield None
+
+
+def joint_proba_on_atoms(p_x: pd.DataFrame, p_y: pd.DataFrame, order_x: pd.DataFrame, order_y: pd.DataFrame,
+                         copula: types.FunctionType) -> pd.DataFrame:
+    """
+        Compute the joint probability on atoms from two marginal probabilities,
+        with a specified order and a specified copula
+        p_x, p_y: pd.Dataframe with columns 'P'.
+        Example
+            P
+        x1  0.5
+        x2  0.2
+        x3  0.3
+
+        order_x, order_y: pd.DataFrame with column 'order'.
+        Example
+                order
+        x3        3
+        x1        1
+        x2        2
+
+        Copula: a function that takes as argument two floats between 0 and 1 and returns the copula on those numbers
+
+        return p_xy: pd.DataFrame. A dataframe with MultiIndex (product of indexes from p_x and p_y) and column 'P'
+        """
+    p_xy = pd.DataFrame(columns=["P"], index=pd.MultiIndex.from_product([p_x.index, p_y.index], names=["X", "Y"]))
+
+    for a_x, a_y in p_xy.index:
+        index = order_x[order_x["order"] < order_x.loc[a_x, "order"]].index
+        sum_x_inf = p_x[index, "P"].sum(axis=0)
+        sum_x_sup = sum_x_inf + float(p_x[a_x, "P"])
+
+        index = order_y[order_y["order"] < order_y.loc[a_y, "order"]].index
+        sum_y_inf = p_y[index, "P"].sum(axis=0)
+        sum_y_sup = sum_y_inf + float(p_y[a_y, "P"])
+
+        p_xy.loc[(a_x, a_y), "P"] = copula(sum_x_sup, sum_y_sup) - copula(sum_x_sup, sum_y_inf) - copula(
+            sum_x_inf, sum_y_sup) + copula(sum_x_inf, sum_y_inf)
+    return p_xy
+
+
+def approximate_robust_credal_set(prob_range_x: pd.DataFrame, prob_range_y: pd.DataFrame,
+                                  order_x_p, order_y_p, copula: types.FunctionType) -> pd.DataFrame:
+    full_events_x = []
+    for k in range(1, len(prob_range_x.index) + 1):
+        full_events_x += [",".join(list(j)) for j in itertools.combinations(prob_range_x.index, k)]
+
+    full_events_y = []
+    for k in range(1, len(prob_range_y.index) + 1):
+        full_events_y += [",".join(list(j)) for j in itertools.combinations(prob_range_y.index, k)]
+
+    multi = pd.MultiIndex.from_product([full_events_x, full_events_y], names=["X", "Y"])
+    robust_df = pd.DataFrame(columns=["P_inf"], index=multi)
+    robust_df["P_inf"] = 1
+
+    generator_x = generator_probability(prob_range_x)
+    generator_y = generator_probability(prob_range_y)
+
+    for p_x in generator_x:
+        for p_y in generator_y:
+            p_xy = joint_proba_on_atoms(p_x, p_y, order_x_p, order_y_p, copula)
+            for x, y in robust_df.index:
+                x_i, y_i = x.split(","), y.split(",")
+                unique_combinations = []
+                # TODO case if len(x_i)< len(y_i)
+                permut = itertools.permutations(x_i, len(y_i))
+                for comb in permut:
+                    zipped = zip(comb, y_i)
+                    unique_combinations += list(zipped)
+
+                robust_df[(x, y), "P"] = min(robust_df[(x, y), "P"], p_xy.loc[unique_combinations, "P"].sum())
+
+    return robust_df
+
+
 if __name__ == "__main__":
+    # Possibility distributions
     poss_x = {"x1": 1, "x2": 0.2, "x3": 0.5}
     poss_y = {"y1": 0.1, "y2": 1, "y3": 0.35}
 
+    # Orderings
     order_x = pd.DataFrame(columns=["order"], index=pd.Index(["x1", "x1,x3", "x1,x2,x3"], name="Focal sets"))
     order_x["order"] = [1, 2, 3]
+    order_x_precise = pd.DataFrame(columns=["order"], index=pd.Index(["x1", "x2", "x3"]))
+    order_x_precise["order"] = [1, 2, 3]
 
-    order_y = pd.DataFrame(columns=["order"], index=pd.Index(["y2", "y2,y3", "y1,y2,y3"], name="Focal sets"))
+    order_y = pd.DataFrame(columns=["order"], index=pd.Index(["y2", "y2,y3", "y1,y2,y3"]))
     order_y["order"] = [2, 3, 1]
+    order_y_precise = pd.DataFrame(columns=["order"], index=pd.Index(["y1", "y2", "y3"]))
+    order_y_precise["order"] = [1, 2, 3]
 
-    print(joint_mass(mass_from_possibility(poss_x), mass_from_possibility(poss_y), order_x, order_y, min_copula))
+    print(probability_range_from_poss(poss_x))
+    # print(joint_mass(mass_from_possibility(poss_x), mass_from_possibility(poss_y), order_x, order_y, min_copula))
