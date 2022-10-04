d8d7cd699152ab7be6eca5ea7bb6d5eec35a0664
Roman MALINOWSKI <roman.malinowski@cnes.fr>
2022-10-04 12:37:21 +0000

    Better looping

--- ordering_focal_set_importance.py
+++ ordering_focal_set_importance.py
@@ -21,42 +21,61 @@ def generator_poss(keys: list) -> dict:
 if __name__ == "__main__":
     output_dir = "/work/scratch/malinoro/simulation_copula/out"
     output_file = "orders.csv"
-    # Possibility distributions
-    possibilities_x = generator_poss(["x1", "x2", "x3", "x4"])
-    possibilities_y = generator_poss(["y1", "y2", "y3", "y4"])
+    
+    #x_space = ["x1", "x2", "x3", "x4"]
+    #y_space = ["y1", "y2", "y3", "y4"]
+    x_space = ["x1", "x2", "x3"]
+    y_space = ["y1", "y2", "y3"]
 
-    order_x_precise = pd.DataFrame(columns=["order"], index=["x1", "x2", "x3", "x4"], data=[1, 2, 3, 4])
-    order_y_precise = pd.DataFrame(columns=["order"], index=["y1", "y2", "y3", "y4"], data=[1, 2, 3, 4])
+    # Possibility distributions
+    possibilities_x = generator_poss(x_space)
+    possibilities_y = generator_poss(y_space)
+
+    order_x_precise = pd.DataFrame(columns=["order"], index=x_space, data=range(1, len(x_space) + 1))
+    order_y_precise = pd.DataFrame(columns=["order"], index=y_space, data=range(1, len(y_space) + 1))
 
     # Initializing the output file
     multi = pd.MultiIndex.from_tuples(list(
-        zip(["poss", "poss", "poss", "poss", "poss", "poss", "poss", "poss", "focal_sets", "focal_sets"],
-            ["x1", "x2", "x3", "x4", "y1", "y2", "y3", "y4", "X", "Y"])), names=["Object", "Space"])
+        zip(["poss"] * (len(x_space) + len(y_space)) + ["focal_sets", "focal_sets"],
+            x_space + y_space + ["X", "Y"])), names=["Object", "Space"])
     final_df = pd.DataFrame(columns=multi)
     final_df.to_csv(os.path.join(output_dir, output_file))
 
-    for poss_x in possibilities_x:
-        for poss_y in possibilities_y:
-            # Finding focal sets
-            nec_x_vanilla = NecessityUnivariate(poss_x)
-            nec_y_vanilla = NecessityUnivariate(poss_y)
+    n_order = 0
 
-            n_order = 0
+    for poss_x in possibilities_x:
+        
+        flag_skip = np.all([k==0. or k==1. for k in poss_x.values()])
+        if flag_skip:
+            continue
+        
+        # Finding focal sets
+        nec_x_vanilla = NecessityUnivariate(poss_x)
+        rob_x = RobustCredalSetUnivariate(nec_x_vanilla, samples_per_interval=5)
+        
+        for poss_y in possibilities_y:
+            
+            flag_skip |= np.all([k==0. or k==1. for k in poss_y.values()])
+            if flag_skip:
+                flag_skip = np.all([k==0. or k==1. for k in poss_x.values()])
+                continue
+
+            # Finding focal sets
+            nec_y_vanilla = NecessityUnivariate(poss_y)
+            rob_y = RobustCredalSetUnivariate(nec_y_vanilla, samples_per_interval=5)
+
+            rob_xy = RobustCredalSetBivariate(rob_x, rob_y, order_x_precise, order_y_precise, min_copula)
+            rob_xy.approximate_robust_credal_set()
 
             flag_order_work = False
 
             for perm_x in itertools.permutations(range(1, len(nec_x_vanilla.mass.index) + 1)):
                 order_x = pd.DataFrame(columns=["order"], index=nec_x_vanilla.mass.index, data=perm_x)
                 nec_x = NecessityUnivariate(poss_x, order_x)
-                rob_x = RobustCredalSetUnivariate(nec_x, samples_per_interval=5)
 
                 for perm_y in itertools.permutations(range(1, len(nec_y_vanilla.mass.index) + 1)):
                     order_y = pd.DataFrame(columns=["order"], index=nec_y_vanilla.mass.index, data=perm_y)
                     nec_y = NecessityUnivariate(poss_y, order_y)
-                    rob_y = RobustCredalSetUnivariate(nec_y, samples_per_interval=5)
-
-                    rob_xy = RobustCredalSetBivariate(rob_x, rob_y, order_x_precise, order_y_precise, min_copula)
-                    rob_xy.approximate_robust_credal_set()
 
                     nec_xy = NecessityBivariate(nec_x, nec_y, min_copula)
 
@@ -64,9 +83,9 @@ if __name__ == "__main__":
                         flag_order_work = True
                         final_df.loc[n_order, [("poss", k) for k in poss_x.keys()]] = poss_x.values()
                         final_df.loc[n_order, [("poss", k) for k in poss_y.keys()]] = poss_y.values()
-                        final_df.loc[n_order, ["focal_sets", "X"]] = "<".join(
+                        final_df.loc[n_order, [("focal_sets", "X")]] = "<".join(
                             order_x.sort_values(axis=0, by=["order"]).index)
-                        final_df.loc[n_order, ["focal_sets", "Y"]] = "<".join(
+                        final_df.loc[n_order, [("focal_sets", "Y")]] = "<".join(
                             order_y.sort_values(axis=0, by=["order"]).index)
                         final_df.to_csv(os.path.join(output_dir, output_file), mode='a', header=False)
                         final_df.drop(axis=0, labels=[n_order], inplace=True)
@@ -80,4 +99,5 @@ if __name__ == "__main__":
 
                 n_order += 1
 
-        possibilities_y = generator_poss(["y1", "y2", "y3"])
+        possibilities_y = generator_poss(y_space)
+    
